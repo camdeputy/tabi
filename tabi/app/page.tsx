c91a29a5e3fd25c3226a3800fa6507f2eed34a8a
Cameron Deputy <camerondeputy@Camerons-Mac-mini.local>
2025-01-30 01:09:31 -0800

    adding navbar

--- tabi/app/page.tsx
+++ tabi/app/page.tsx
@@ -1,8 +1,31 @@
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import Link from "next/link"
+
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-lg text-black">Welcome to Tabi</h1>
-    </div>
+    <main>
+        <NavigationMenu className="flex p-4">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/auth/sign-in" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Sign In
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/auth/sign-up" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Sign Up
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      <div>
+        <h1 className="text-lg text-black">Welcome to Tabi</h1>
+      </div>
+    </main>
   )
 }
